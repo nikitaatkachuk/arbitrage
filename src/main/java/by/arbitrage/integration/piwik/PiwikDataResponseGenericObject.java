629f1645c0d58e42c38054b38b939f976e1804bd
Nick <nikitaatkachuk@gmail.com>
2015-06-18 14:57:28 +0300

    start piwik integration

--- src/main/java/by/arbitrage/integration/piwik/PiwikDataResponseGenericObject.java
+++ src/main/java/by/arbitrage/integration/piwik/PiwikDataResponseGenericObject.java
@@ -1,11 +1,87 @@
 package by.arbitrage.integration.piwik;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Created with IntelliJ IDEA.
  * User: nikita tkachuk
  * Date: 6/18/2015
  * Time: 6:38 AM
  */
+@JsonAutoDetect
 public class PiwikDataResponseGenericObject
 {
+
+	/**
+	 *  Number of unique visitors
+	 */
+
+	@JsonProperty("nb_uniq_visitors")
+	public long uniqueVisitorNumber;
+
+	/**
+	 * Number of Visits (30 min of inactivity considered a new visit)
+	 */
+
+	@JsonProperty("nb_visits")
+	public long numberOfVisits;
+
+	/**
+	 * Number of unique active users (visitors with a known User ID).
+	 * If you are not using User ID then this metric will be set to zero
+	 */
+
+	@JsonProperty("nb_users")
+	public long usersNumber;
+
+	/**
+	 * Number of actions (page views, outlinks and downloads)
+	 */
+
+	@JsonProperty("nb_actions")
+	public long actionsNumber;
+
+	/**
+	 * Total time spent, in seconds
+	 */
+
+	@JsonProperty("sum_visit_length")
+	public long timeSpent;
+
+	/**
+	 * Number of visits that bounced (viewed only one page)
+	 */
+
+	@JsonProperty("bounce_count")
+	public long bounceCount;
+
+	/**
+	 * Maximum number of actions in a visit
+	 */
+
+	@JsonProperty("max_actions")
+	public long maxActions;
+
+	/**
+	 * Number of visits that converted a goal
+	 */
+
+	@JsonProperty("nb_visits_converted")
+	public long visitsToGoal;
+
+	/**
+	 * Number of goal conversions
+	 */
+
+	@JsonProperty("nb_conversions")
+	public long conversion;
+
+	/**
+	 * Total revenue of goal conversions
+	 */
+
+	@JsonProperty("revenue")
+	public long revenue ;
+
 }
--- src/main/java/by/arbitrage/integration/piwik/PiwikResponseToObjectMapper.java
+++ src/main/java/by/arbitrage/integration/piwik/PiwikResponseToObjectMapper.java
@@ -1,5 +1,17 @@
 package by.arbitrage.integration.piwik;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Created with IntelliJ IDEA.
  * User: nikita tkachuk
@@ -8,4 +20,47 @@ package by.arbitrage.integration.piwik;
  */
 public class PiwikResponseToObjectMapper
 {
+	public static PiwikDataResponseGenericObject responseToObject(String url)
+	{
+		PiwikDataResponseGenericObject result = new PiwikDataResponseGenericObject();
+		URL destination = null;
+		HttpURLConnection connection = null;
+		try
+		{
+			destination = new URL(url);
+			connection = (HttpURLConnection) destination.openConnection();
+			connection.setInstanceFollowRedirects(false);
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(600);
+			String s;
+			try(BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream())))
+			{
+				while((s = reader.readLine()) != null);
+			}
+			ObjectMapper mapper = new ObjectMapper() ;
+			StringWriter writer = new StringWriter();
+			writer.write(s);
+			mapper.writeValue(writer, result);
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace(); //todo: change me
+		}
+		catch (ProtocolException e)
+		{
+			e.printStackTrace(); //todo: change me
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace(); //todo: change me
+		}
+		finally
+		{
+			if(connection != null)
+			{
+				connection.disconnect();
+			}
+		}
+		return result;
+	}
 }
