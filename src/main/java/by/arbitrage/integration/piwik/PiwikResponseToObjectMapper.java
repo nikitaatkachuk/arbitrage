3800e971aacff67efca849073a985e91ce01cbd8
Nick <nikitaatkachuk@gmail.com>
2015-06-18 14:55:47 +0300

    Start piwik integration

---
+++ src/main/java/by/arbitrage/integration/piwik/PiwikResponseToObjectMapper.java
@@ -0,0 +1,11 @@
+package by.arbitrage.integration.piwik;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikita tkachuk
+ * Date: 6/18/2015
+ * Time: 6:43 AM
+ */
+public class PiwikResponseToObjectMapper
+{
+}
